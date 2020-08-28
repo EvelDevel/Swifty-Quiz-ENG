578aa6bf1e136a79a1415222b2250f80da8149ea
Evel-Devel <zhenka_nikitin@mail.ru>
2020-08-28 09:44:37 +0700

    Финальная редактура Универсальных шаблонов.

--- Swifty-Quiz-ENG/Database/Questions/The Basics /09 row/GenericsSet.swift
+++ Swifty-Quiz-ENG/Database/Questions/The Basics /09 row/GenericsSet.swift
@@ -9,9 +9,9 @@ import Foundation
 class GenericsSet {
 	static func getQuestions() -> [Question] {
 		return [
-			Question(question: ["What can you create with generic?",
+			Question(question: ["What can you create with generics?",
 								"What do generics help you to implement?",
-								"Generics helps create ___"],
+								"Generics help you create ___"],
 					 image: "",
 					 optionA: "Flexible, reusable functions",
 					 optionB: "Collections",
@@ -22,28 +22,28 @@ class GenericsSet {
 
 			Question(question: ["Are generics used in Swift standard libraries?",
 								"Much of the Swift standard library is built with generic code. Is that true?",
-								"Do you think that generics are used in the Swift standard libraries?"],
+								"Are generics common in the Swift standard libraries?"],
 					 image: "",
 					 optionA: "Yes",
 					 optionB: "No",
 					 optionC: "",
 					 optionD: "",
 					 questionId: 2103,
-					 helpText: "Generics are one of the most powerful features of Swift, and much of the Swift standard library is built with generic code. In fact, you’ve been using generics throughout the Language Guide, even if you didn’t realize it."),
+					 helpText: "Generics are one of the most powerful features of Swift, and much of the Swift standard library is built with generic code. Most probably, you’ve already been using generics, even if you didn’t realize it."),
 
-			Question(question: ["Are Swift Array type is generic collection?",
-								"Are Swift Dictionarie types is generic collection?",
-								"Are Swift’s Array and Dictionary types are both generic collections?"],
+			Question(question: ["Is Swift’s Array type a generic collection?",
+								"Does Swift’s Dictionary type represent a generic collection?",
+								"Are Swift’s Array and Dictionary types both generic collections?"],
 					 image: "",
 					 optionA: "Yes",
 					 optionB: "No",
 					 optionC: "Only Dictionary",
 					 optionD: "Only Array",
 					 questionId: 2104,
-					 helpText: "Swift’s Array and Dictionary types are both generic collections. You can create an array that holds Int values, or an array that holds String values, or indeed an array for any other type that can be created in Swift. Similarly, you can create a dictionary to store values of any specified type, and there are no limitations on what that type can be."),
+					 helpText: "Swift’s Array and Dictionary types are both generic collections. You can create an array that holds Int values, or an array that holds String values, or an array for any other type that can be created in Swift. Similarly, you can create a dictionary to store values of any specified type, and there are no limitations on what that type can be."),
 
-			Question(question: ["With which type can generic functions work?",
-								"Generic functions can work with this type"],
+			Question(question: ["Which types can generic functions work with?",
+								"Generic functions can work with the following type(s)"],
 					 image: "",
 					 optionA: "With any type",
 					 optionB: "With integers",
@@ -52,18 +52,16 @@ class GenericsSet {
 					 questionId: 2105,
 					 helpText: "Generic functions can work with any type."),
 
-			/// В 2106 последнее предложение хелпа - самописное и вручную переведенное
-
-			Question(question: ["The generic functions usually uses this placeholder type name"],
+			Question(question: ["Pick the placeholder type name that generic functions usually use"],
 					 image: "",
 					 optionA: "<T>",
 					 optionB: "T",
 					 optionC: "(T)",
 					 optionD: "",
 					 questionId: 2106,
-					 helpText: "The generic version of the function uses a placeholder type name (<T>) instead of an actual type name (such as Int, String, or Double). The placeholder type name doesn’t say anything about what T must be, but it does say that both a and b must be of the same type T, whatever T represents. The actual type to use in place of T is determined each time the function is called. When there is no meaningful relationship between a type parameter and a function, then by tradition, individual letters such as T, U, V become names."),
+					 helpText: "The generic version of the function uses a placeholder type name (<T>) instead of an actual type name (such as Int, String, or Double). The placeholder type name doesn’t say anything about what T must be, but it does say that all function’s parameters must be of the same type T. The current type that T represents is determined each time the function is called. When there is no meaningful relationship between a type parameter and a function, then by tradition, individual letters such as T, U, V become names."),
 
-			Question(question: ["At what point the type T is inferred from the types of values passed to the function?"],
+			Question(question: ["At what point the type to use for T is inferred from the types of values passed to the function?"],
 					 image: "",
 					 optionA: "Each time the function is called",
 					 optionB: "When the function body runs",
@@ -72,7 +70,7 @@ class GenericsSet {
 					 questionId: 2107,
 					 helpText: "Each time the function is called, the type to use for T is inferred from the types of values passed to the function."),
 
-			Question(question: ["What is the type parameter here?"],
+			Question(question: ["Where is the type parameter here?"],
 					 image: "Generics8",
 					 optionA: "<T>",
 					 optionB: "_ a: inout T",
@@ -104,7 +102,7 @@ class GenericsSet {
 
 			Question(question: ["Can we define our own generic types?",
 								"Can we define our own generic types in addition to generic functions?",
-								"Can you define generic types in Swift?"],
+								"Can you define a generic type in Swift?"],
 					 image: "",
 					 optionA: "Yes",
 					 optionB: "No",
@@ -124,7 +122,7 @@ class GenericsSet {
 					 questionId: 2112,
 					 helpText: "Yes. When you extend a generic type, you don’t provide a type parameter list as part of the extension’s definition. Instead, the type parameter list from the original type definition is available within the body of the extension, and the original type parameter names are used to refer to the type parameters from the original definition."),
 
-			Question(question: ["When you extend a generic type, do you provide a type parameter list as part of the extension’s definition?"],
+			Question(question: ["Do you provide a type parameter list as part of the extension’s definition while extending a generic type,"],
 					 image: "",
 					 optionA: "No",
 					 optionB: "Yes",
@@ -143,7 +141,7 @@ class GenericsSet {
 					 helpText: "Sometimes it's useful to enforce certain type constraints on the types that can be used with generic functions and generic types. Type constraints specify that a type parameter must inherit from a specific class, or conform to a particular protocol or protocol composition. For example, Swift’s Dictionary type places a limitation on the types that can be used as keys for a dictionary. As described in Dictionaries, the type of a dictionary’s keys must be hashable. That is, it must provide a way to make itself uniquely representable."),
 
 			Question(question: ["Can we define our own type constraints when creating custom generic types?",
-								"You can define your own type constraints when creating custom generic types. Is that true?"],
+								"Is that true that you can define your own type constraints when creating custom generic types?"],
 					 image: "",
 					 optionA: "Yes",
 					 optionB: "No",
@@ -152,7 +150,7 @@ class GenericsSet {
 					 questionId: 2115,
 					 helpText: "You can define your own type constraints when creating custom generic types, and these constraints provide much of the power of generic programming. Abstract concepts like Hashable characterize types in terms of their conceptual characteristics, rather than their concrete type."),
 
-			Question(question: ["Which type parameter has a correct type constraint that requires T to be a subclass of SomeClass"],
+			Question(question: ["Which of the following options is a valid type constraint that requires T to be a subclass of SomeClass"],
 					 image: "",
 					 optionA: "<T: SomeClass>",
 					 optionB: "<T> - SomeClass",
@@ -161,8 +159,7 @@ class GenericsSet {
 					 questionId: 2116,
 					 helpText: "You write type constraints by placing a single class or protocol constraint after a type parameter’s name, separated by a colon, as part of the type parameter list. func someFunction<T: SomeClass, U: SomeProtocol>(someT: T, someU: U) { // function body goes here }. The hypothetical function above has two type parameters. The first type parameter, T, has a type constraint that requires T to be a subclass of SomeClass. The second type parameter, U, has a type constraint that requires U to conform to the protocol SomeProtocol."),
 
-			/// Пожалуйста эту формулировку поплотнее проверь :)
-			Question(question: ["What type constraint must be assigned to the type parameter so that it can implement support for the equality and inequality operators?",
+			Question(question: ["What type constraint must be assigned to a generic type <T> to implement support for the equal to (==) and the not equal to (!=) operators?",
 								"Which protocol requires any conforming type to implement the equal to operator (==) and the not equal to operator (!=) to compare any two values of that type?"],
 					 image: "",
 					 optionA: "Equatable protocol",
@@ -170,9 +167,9 @@ class GenericsSet {
 					 optionC: "Any protocol",
 					 optionD: "Arithmetic protocol",
 					 questionId: 2117,
-					 helpText: "The Swift standard library defines a protocol called Equatable, which requires any conforming type to implement the equal to operator (==) and the not equal to operator (!=) to compare any two values of that type. All of Swift’s standard types automatically support the Equatable protocol. Any type that is Equatable can be used safely with function, because it’s guaranteed to support the equal to operator."),
+					 helpText: "The Swift standard library defines a protocol called Equatable, which requires any conforming type to implement the equal to operator (==) and the not equal to operator (!=) to compare any two values of that type. All of Swift’s standard types automatically support the Equatable protocol. Any type that is Equatable can be used safely with function because it’s guaranteed to support the equal to operator."),
 
-			Question(question: ["Which keyword declare one or more associated types as part of the protocol’s definition?"],
+			Question(question: ["Which keyword declares one or more associated types as part of the protocol’s definition?"],
 					 image: "",
 					 optionA: "associatedtype",
 					 optionB: "mutating",
@@ -180,6 +177,7 @@ class GenericsSet {
 					 optionD: "append",
 					 questionId: 2118,
 					 helpText: "When defining a protocol, it’s sometimes useful to declare one or more associated types as part of the protocol’s definition. An associated type gives a placeholder name to a type that is used as part of the protocol. The actual type to use for that associated type isn’t specified until the protocol is adopted. Associated types are specified with the associatedtype keyword.")
+			
 		]
 	}
 }
