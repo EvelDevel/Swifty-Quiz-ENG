163eb7cbe9548f807eff428a95ecc9047483e4a9
Evel-Devel <zhenka_nikitin@mail.ru>
2020-09-03 19:49:40 +0700

    Точка старта подключения встроенной покупки. К этому месту можно откатиться.

--- Swifty-Quiz-ENG/Helpers/AdaptiveInterface.swift
+++ Swifty-Quiz-ENG/Helpers/AdaptiveInterface.swift
@@ -83,7 +83,49 @@ class AdaptiveInterface {
 
 		} else {
 			/// ipads
+		}
+	}
 
+	/// Адаптация экрана "Покупки"
+	func setUnlockPageInterface(view: UIView,
+							  IapSupportLabel: UILabel,
+							  IapMainTextLabel: UILabel,
+							  IapGoodLuckLabel: UILabel,
+							  getYourFullAccessHeight: NSLayoutConstraint,
+							  topMargin: NSLayoutConstraint,
+							  textTopMargin: NSLayoutConstraint,
+							  labelsTrailingSpace: NSLayoutConstraint,
+							  labelsLeadingSpace: NSLayoutConstraint) {
+
+		if view.frame.size.width <= 320 {
+			IapSupportLabel.font = UIFont.systemFont(ofSize: 14.0, weight: .regular)
+			IapMainTextLabel.font = UIFont.systemFont(ofSize: 15.0, weight: .thin)
+			IapGoodLuckLabel.font = UIFont.systemFont(ofSize: 14.0, weight: .regular)
+			getYourFullAccessHeight.constant = 170
+			topMargin.constant = 20
+			textTopMargin.constant = 5
+			labelsTrailingSpace.constant = 20
+			labelsLeadingSpace.constant = 20
+
+		} else if view.frame.size.width <= 410 {
+			IapSupportLabel.font = UIFont.systemFont(ofSize: 15.0, weight: .regular)
+			IapMainTextLabel.font = UIFont.systemFont(ofSize: 18.0, weight: .thin)
+			IapGoodLuckLabel.font = UIFont.systemFont(ofSize: 15.0, weight: .regular)
+			getYourFullAccessHeight.constant = 240
+			topMargin.constant = 20
+			textTopMargin.constant = 5
+			labelsTrailingSpace.constant = 24
+			labelsLeadingSpace.constant = 24
+
+		} else {
+			IapSupportLabel.font = UIFont.systemFont(ofSize: 18.0, weight: .regular)
+			IapMainTextLabel.font = UIFont.systemFont(ofSize: 20, weight: .thin)
+			IapGoodLuckLabel.font = UIFont.systemFont(ofSize: 18, weight: .regular)
+			getYourFullAccessHeight.constant = 250
+			topMargin.constant = 40
+			textTopMargin.constant = 20
+			labelsTrailingSpace.constant = 30
+			labelsLeadingSpace.constant = 30
 		}
 	}
 }
--- Swifty-Quiz-ENG/Helpers/IAP/InAppPurchaseViewController.swift
+++ Swifty-Quiz-ENG/Helpers/IAP/InAppPurchaseViewController.swift
@@ -18,8 +18,8 @@ class InAppPurchaseViewController: UIViewController {
 	@IBOutlet weak var labelsLeadingSpace: NSLayoutConstraint!
 	@IBOutlet weak var textTopMargin: NSLayoutConstraint!
 
-	/// Главный параметр "Была ли покупка?"
 	static var purchased = false
+	private var adaptiveInterface = AdaptiveInterface()
 
 	let shadow = ShadowsHelper()
 
@@ -36,33 +36,14 @@ class InAppPurchaseViewController: UIViewController {
 	}
 
 	func setTheInterfaceRight() {
-		if view.frame.size.width <= 320 {
-			IapSupportLabel.font = UIFont.systemFont(ofSize: 14.0, weight: .regular)
-			IapMainTextLabel.font = UIFont.systemFont(ofSize: 15.0, weight: .thin)
-			IapGoodLuckLabel.font = UIFont.systemFont(ofSize: 14.0, weight: .regular)
-			getYourFullAccessHeight.constant = 170
-			topMargin.constant = 20
-			textTopMargin.constant = 5
-			labelsTrailingSpace.constant = 20
-			labelsLeadingSpace.constant = 20
-		} else if view.frame.size.width <= 410 {
-			IapSupportLabel.font = UIFont.systemFont(ofSize: 15.0, weight: .regular)
-			IapMainTextLabel.font = UIFont.systemFont(ofSize: 18.0, weight: .thin)
-			IapGoodLuckLabel.font = UIFont.systemFont(ofSize: 15.0, weight: .regular)
-			getYourFullAccessHeight.constant = 240
-			topMargin.constant = 20
-			textTopMargin.constant = 5
-			labelsTrailingSpace.constant = 24
-			labelsLeadingSpace.constant = 24
-		} else {
-			IapSupportLabel.font = UIFont.systemFont(ofSize: 18.0, weight: .regular)
-			IapMainTextLabel.font = UIFont.systemFont(ofSize: 20, weight: .thin)
-			IapGoodLuckLabel.font = UIFont.systemFont(ofSize: 18, weight: .regular)
-			getYourFullAccessHeight.constant = 250
-			topMargin.constant = 40
-			textTopMargin.constant = 20
-			labelsTrailingSpace.constant = 30
-			labelsLeadingSpace.constant = 30
-		}
+		adaptiveInterface.setUnlockPageInterface(view: view,
+												 IapSupportLabel: IapSupportLabel,
+												 IapMainTextLabel: IapMainTextLabel,
+												 IapGoodLuckLabel: IapGoodLuckLabel,
+												 getYourFullAccessHeight: getYourFullAccessHeight,
+												 topMargin: topMargin,
+												 textTopMargin: textTopMargin,
+												 labelsTrailingSpace: labelsTrailingSpace,
+												 labelsLeadingSpace: labelsLeadingSpace)
 	}
 }
