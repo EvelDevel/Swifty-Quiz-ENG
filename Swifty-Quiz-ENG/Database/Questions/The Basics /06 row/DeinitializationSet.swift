b22e3b5af4585ebd462355385768834b8920ac35
Evel-Devel <zhenka_nikitin@mail.ru>
2020-09-03 20:54:47 +0700

    Чистовой перевод "Деинициализаторов". Присвоил сториборду с покупкой свой айдишник.

--- Swifty-Quiz-ENG/Database/Questions/The Basics /06 row/DeinitializationSet.swift
+++ Swifty-Quiz-ENG/Database/Questions/The Basics /06 row/DeinitializationSet.swift
@@ -9,8 +9,8 @@ import Foundation
 class DeinitializationSet {
 	static func getQuestions() -> [Question] {
 		return [
-			Question(question: ["You write deinitializers with the that keyword",
-								"Keyword for deinitialization"],
+			Question(question: ["You write deinitializers with that keyword",
+								"The keyword used for deinitialization"],
 					 image: "",
 					 optionA: "deinit",
 					 optionB: "init",
@@ -19,40 +19,40 @@ class DeinitializationSet {
 					 questionId: 1402,
 					 helpText: "A deinitializer is called immediately before a class instance is deallocated. You write deinitializers with the deinit keyword, similar to how initializers are written with the init keyword. Deinitializers are only available on class types."),
 
-			Question(question: ["For what types are deinitializers available for?",
+			Question(question: ["Which of the following options can make use of deinitializers?",
 								"Deinitializers are only available for these types"],
 					 image: "",
 					 optionA: "Class types",
-					 optionB: "Structures types",
-					 optionC: "Enumerations types",
+					 optionB: "Structure types",
+					 optionC: "Enumeration types",
 					 optionD: "All types",
 					 questionId: 1403,
 					 helpText: "Deinitializers are only available on class types."),
 
-			Question(question: ["Class definitions can have that many deinitializers per class",
-								"How many deinitializers can have a class definitions?"],
+			Question(question: ["Pick the correct maximum number of deinitializers available for a class definition",
+								"How many deinitializers can a class definition have?"],
 					 image: "",
 					 optionA: "One",
 					 optionB: "Two",
 					 optionC: "Three",
-					 optionD: "As many as we needed",
+					 optionD: "As many as needed",
 					 questionId: 1404,
 					 helpText: "Class definitions can have at most one deinitializer per class."),
 
-			Question(question: ["Does deinitializer need the parentheses?",
-								"The deinitializer can take parameters. Is that true?",
-								"We write deinitializers with parentheses. Is that true?"],
+			Question(question: ["Do deinitializers require parentheses?",
+								"Deinitializer can take parameters. Is that true?",
+								"We write deinitializers with parentheses. Is that correct?"],
 					 image: "",
 					 optionA: "No",
 					 optionB: "Yes",
 					 optionC: "",
 					 optionD: "",
 					 questionId: 1405,
-					 helpText: "The deinitializer does not take any parameters and is written without parentheses."),
+					 helpText: "Deinitializers don’t take any parameters and are written without parentheses."),
 
 			Question(question: ["Deinitializers are called automatically, right after the instance deallocation takes place. Is that true?",
-								"We need to call deinitializers manually. Is that true?",
-								"Deinitializers need to be called manually, right after instance deallocation takes place. Is that true?"],
+								"Is it required to call deinitializers manually?",
+								"Deinitializers are called manually, right after instance deallocation takes place. Is that true?"],
 					 image: "",
 					 optionA: "No",
 					 optionB: "Yes",
@@ -62,7 +62,7 @@ class DeinitializationSet {
 					 helpText: "Deinitializers are called automatically, just before instance deallocation takes place."),
 
 			Question(question: ["Are you allowed to call a deinitializer by yourself?",
-								"You are allowed to call a deinitializer yourself. Is that true?"],
+								"You can call a deinitializer yourself. Is that true?"],
 					 image: "",
 					 optionA: "No",
 					 optionB: "Yes",
@@ -82,9 +82,9 @@ class DeinitializationSet {
 					 questionId: 1408,
 					 helpText: "Superclass deinitializers are inherited by their subclasses, and the superclass deinitializer is called automatically at the end of a subclass deinitializer implementation. Superclass deinitializers are always called, even if a subclass does not provide its own deinitializer."),
 
-			Question(question: ["Can deinitializer access all properties of the instance?",
-								"Can deinitializer modify its behavior based on properties of the instance?",
-								"A deinitializer can access all properties of the instance. Is that true?"],
+			Question(question: ["Can deinitializers access the properties of an instance?",
+								"Can deinitializers modify its own behavior based on properties of an instance?",
+								"A deinitializer can access all the properties of an instance. Is that correct?"],
 					 image: "",
 					 optionA: "Yes",
 					 optionB: "No",
@@ -92,6 +92,8 @@ class DeinitializationSet {
 					 optionD: "",
 					 questionId: 1409,
 					 helpText: "Because an instance is not deallocated until after its deinitializer is called, a deinitializer can access all properties of the instance it is called on and can modify its behavior based on those properties (such as looking up the name of a file that needs to be closed).")
+
+
 		]
 	}
 }
