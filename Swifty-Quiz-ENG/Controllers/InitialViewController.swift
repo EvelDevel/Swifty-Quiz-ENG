5d151eef76d730797a6ab5569169acbc312d6b3e
Evel-Devel <zhenka_nikitin@mail.ru>
2020-09-23 13:31:55 +0700

    Чистовой перевод Перечислений

--- Swifty-Quiz-ENG/Controllers/InitialViewController.swift
+++ Swifty-Quiz-ENG/Controllers/InitialViewController.swift
@@ -7,7 +7,6 @@ import UIKit
 // MARK: TODO - Баги и доработки
 /// Добавить логгер на ин-апп
 /// Социальные фичи (поделиться результатом)
-/// Покупка не отрабатывает на 14.0 оси 
 
 class InitialViewController: UIViewController {
 
